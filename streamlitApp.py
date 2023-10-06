3ff6e184a0900c46527c8b203d396d66afbd00d7
0-lucas <luccleandro@hotmail.com>
2023-10-06 18:09:23 -0300

    Tentativa de fix das dependências do streamlit.

--- streamlitApp.py
+++ streamlitApp.py
@@ -1,24 +1,23 @@
-from tensorflow import keras
+from keras.models import load_model
 import streamlit as st
 import cv2
 import numpy as np
-
-modelo = keras.models.load_model("mymodel.keras")
+st.set_page_config(layout="wide")
+modelo = load_model("mymodel.keras")
 
 class_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S','T','U',
                'V', 'W', 'X', 'Y', 'Z', 'apagar', 'vazio', 'espaço']
 class_mapping = dict(enumerate(class_names))
 
-webcam = cv2.VideoCapture(1)
+webcam = cv2.VideoCapture(0)
 
 col1, col2 = st.columns([0.6, 0.4])
 
+col1.subheader("Classificação de Linguagem de Sinais - ASL")
+frame_placeholder = col1.empty()
+
 col2.subheader("Tabela de conversão alfabética para ASL")
 col2.image(cv2.imread("tabela_conversao.jpg"), use_column_width="always")
-
-col1.subheader("Classificação de Linguagem de Sinais - ASL")
-
-frame_placeholder = col1.empty()
 
 while True:
     # Lê um único frame.
